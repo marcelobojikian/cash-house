65fcb6e49b0a53fedb008b938feeb6ee0acc32f4
marcelobojikian <marcelo.bojikian@gmail.com>
2020-03-10 21:30:52 +0000

    Test converter

---
+++ backend/src/test/java/br/com/housecash/backend/converter/TransactionConverterTest.java
@@ -0,0 +1,42 @@
+package br.com.housecash.backend.converter;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.housecash.backend.App;
+import br.com.housecash.backend.model.Transaction.Action;
+import br.com.housecash.backend.model.Transaction.Status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
+public class TransactionConverterTest {
+
+	@Autowired
+	ConversionService conversionService;
+
+	@Test
+	public void whenConvertStringToStatus_thenSuccess() {
+
+		assertEquals(conversionService.convert("created", Status.class),Status.CREATED);
+		assertEquals(conversionService.convert("sended", Status.class),Status.SENDED);
+		assertEquals(conversionService.convert("finished", Status.class),Status.FINISHED);
+		assertEquals(conversionService.convert("canceled", Status.class),Status.CANCELED);
+		assertEquals(conversionService.convert("deleted", Status.class),Status.DELETED);
+		
+	}
+
+	@Test
+	public void whenConvertStringToAction_thenSuccess() {
+
+		assertEquals(conversionService.convert("deposit", Action.class),Action.DEPOSIT);
+		assertEquals(conversionService.convert("withdraw", Action.class),Action.WITHDRAW);
+		
+	}
+
+}
