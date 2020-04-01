06639bba45ccbcadce85610aaf19d2fdfaa6ec96
marcelobojikian <marcelo.bojikian@gmail.com>
2020-04-01 06:39:51 +0100

    Fix eclipse files

--- backend/src/main/java/br/com/housecash/backend/service/LocaleService.java
+++
@@ -1,9 +0,0 @@
-package br.com.housecash.backend.service;
-
-public interface LocaleService {
-
-	public String getMessage(String code);
-
-	public String getMessage(String code, Object... args);
-
-}
